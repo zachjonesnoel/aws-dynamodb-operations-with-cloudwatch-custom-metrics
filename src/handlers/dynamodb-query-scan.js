5c718a1357a6a6224e554cb1d34dda1ad8a0052d
Jones Zachariah Noel <joneszachariahnoel@gmail.com>
2021-08-22 11:22:36 +0000

    Initial commit

---
+++ src/handlers/dynamodb-query-scan.js
@@ -0,0 +1,58 @@
+/**
+ * A Lambda function that queries DynamoDB and updates CloudWatch Custom metrics with the time taken for each DynamoDB operation.
+ */
+const AWS = require("aws-sdk")
+const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+const cloudwatch = new AWS.CloudWatch()
+
+const custom_cloudwatch_metric = async(metric, value) => {
+    await cloudwatch.putMetricData({
+        'MetricData': [{
+            'MetricName': 'DynamoDBOperations',
+            'Dimensions': [{
+                'Name': 'OPERATION',
+                'Value': metric
+            }],
+            'Unit': 'Milliseconds',
+            'Value': value
+        }, ],
+        'Namespace': 'DynamoDB Queries and Scans'
+    }).promise()
+}
+
+exports.dynamoQueryAndScan = async(event, context) => {
+    let start_time = Date.now()
+    await scan_dynamo()
+    await query_dynamo()
+    let end_time = Date.now()
+    console.log("Lambda execution with DynamoDB Scan and Query : ", end_time - start_time)
+    return {
+        "status": 200
+    }
+
+}
+
+const scan_dynamo = async() => {
+    let start_time = Date.now()
+    let scanResponse = await dynamodb.scan({
+        "TableName": "cars-demo"
+    }).promise()
+    let end_time = Date.now()
+    console.log("DynamoDB Scan : ", end_time - start_time)
+    await custom_cloudwatch_metric("SCAN", end_time - start_time)
+    return "SUCCESS"
+}
+
+const query_dynamo = async() => {
+    let start_time = Date.now()
+    let queryResponse = await dynamodb.query({
+        TableName: 'cars-demo',
+        IndexName: 'origin-index',
+        KeyConditionExpression: 'origin = :origin',
+        ExpressionAttributeValues: { ':origin': { "S": "USA" } },
+    }).promise()
+    let end_time = Date.now()
+    console.log("DynamoDB Query : ", end_time - start_time)
+    await custom_cloudwatch_metric("QUERY", end_time - start_time)
+    return "SUCCESS"
+}
